2784e46c6455186a6ab81d4fa6fe9b0632531274
manalsal <menalglt@gmail.com>
2023-04-29 13:41:29 -0700

    singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,42 +1,15 @@
 #include <stdio.h>
 
-
-
-
-
-
-
-
-
 /**
-
-
 * first - prints a function that is executed before main
-
-
 * Return: no return.
-
-
 */
-
-
-
-
-
-
 
 void __attribute__ ((constructor)) first()
 
-
 {
-
-
 	printf("You're beat! and yet, you must allow,\n");
-
 
 	printf("I bore my house upon my back!\n");
 
-
 }
-
-
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,66 +1,23 @@
 #include <stdlib.h>
-
-
-
-
 #include "lists.h"
-
-
-
-
-
-
-
 /**
-
-
 * free_list - frees a linked list
-
-
-* -head: list_t list to be freed
-
-
+* @head: list_t list to be freed
 * Return: no return.
-
-
 */
-
-
 void free_list(list_t *head)
-
-
 {
-
-
 list_t *temp;
-
-
-
-
-
-
-
 while (head)
-
-
 {
-
-
 temp = head->next;
-
 
 free(head->str);
 
-
 free(head);
-
 
 head = temp;
 
-
 }
 
-
 }
-
-
